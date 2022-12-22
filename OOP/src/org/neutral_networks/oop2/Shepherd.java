298ac508fc2117268ea8b34bfaba7fd98840180d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-22 22:23:35 +0600

    Create Shepherd.java

---
+++ OOP/src/org/neutral_networks/oop2/Shepherd.java
@@ -0,0 +1,7 @@
+package org.neutral_networks.oop2;
+
+public class Shepherd extends Dog{
+    public Shepherd(String name, String color, int legs, boolean hasTale) {
+        super(name, color, legs, hasTale);
+    }
+}
