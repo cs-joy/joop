2472df9ca93e86ee9c33a5194d803f3144227c2f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-24 21:33:37 +0600

    Create Organ.java

---
+++ OOP/src/org/neutral_networks/oop_challange/Organ.java
@@ -0,0 +1,27 @@
+package org.neutral_networks.oop_challange;
+
+public class Organ {
+    private String name;
+    private String medicalCondition;
+
+    public Organ(String name, String medicalCondition) {
+        this.name = name;
+        this.medicalCondition = medicalCondition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public void setMedicalCondition(String medicalCondition) {
+        this.medicalCondition = medicalCondition;
+    }
+}
