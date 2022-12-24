abedbcb0f47038da2aad849873d7a2d717501121
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-24 23:36:19 +0600

    Update definingArrays.java

--- OOP/src/org/neutral_networks/collections/definingArrays.java
+++ OOP/src/org/neutral_networks/collections/definingArrays.java
@@ -11,9 +11,24 @@ public class definingArrays {
         // and one of the limitation is
         // we can't add new values in a array
         // for example
-        //department[6] = "MBA"; // generate error: ArrayIndexOutOfBoundsException
+        //department[6] = "BANGLA"; // generate error: ArrayIndexOutOfBoundsException
         // we can not do that so this is the one of limitation to define the array in this way
         //System.out.println(department[5]);
+
+        // so how we append or add new elements in the array
+        // there have one way to do that, that is
+        // created a new array and copy those elements to the new array after that append your new elements to the new array
+
+        // create new array with new index
+        String[] updateDepartment = new String[7];
+        // copy all elements to the new array
+        for(int i=0; i<department.length; i++) {
+            updateDepartment[i] = department[i];
+        }
+
+        // now append the value at index 6
+        updateDepartment[6] = "BANGLA";
+        System.out.println(updateDepartment[6]);
     }
 
 }
