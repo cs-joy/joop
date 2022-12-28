9a4931c547601a754dcfcca9d07e78a62734d4a2
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-28 19:31:39 +0600

    Update secondTest.java

--- OOP/src/org/neutral_networks/staticKeyword/secondTest.java
+++ OOP/src/org/neutral_networks/staticKeyword/secondTest.java
@@ -1,4 +1,17 @@
 package org.neutral_networks.staticKeyword;
 
 public class secondTest {
+    private int id;
+    private int age;
+    private class innerClass {
+        private String name;
+
+        public innerClass(String name) {
+            this.name = name;
+        }
+
+        private void printAge() {
+            System.out.println("Age: " + age);
+        }
+    }
 }
