23ed48983a41f6d1484c31d4901b1b49bbaa0057
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-28 18:56:54 +0600

    Update Main.java

--- OOP/src/org/neutral_networks/staticKeyword/Main.java
+++ OOP/src/org/neutral_networks/staticKeyword/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         TestClass.setName("Mike");
         // or
-        TestClass.name = "Jim";
+        //TestClass.name = "Jim";
         TestClass testClass = new TestClass(20, "White");
         //testClass.setName("Mike"); // this is not a right way to set the value to a static variable
         System.out.println("Name: " + testClass.getName());
+
+        // create another object
+        TestClass secObj = new TestClass(27, "Black");
+        System.out.println(secObj.name); // Mike is printed
     }
 }
