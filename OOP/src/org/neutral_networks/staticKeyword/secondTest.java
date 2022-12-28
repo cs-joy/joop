36fa8cc55de9cf621d867b4797d2ebd721acd69c
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-28 22:03:54 +0600

    Update secondTest.java

--- OOP/src/org/neutral_networks/staticKeyword/secondTest.java
+++ OOP/src/org/neutral_networks/staticKeyword/secondTest.java
@@ -1,9 +1,10 @@
 package org.neutral_networks.staticKeyword;
 
-public class secondTest {
+public class secondTest { // here modifier "private" and "static"  not allowed here
     private int id;
     private int age;
-    private class innerClass {
+    private class innerClass { // here is private and static both modifier allowed here in inner class definition,
+        // private static class innerClass - allowed here
         private String name;
 
         public innerClass(String name) {
