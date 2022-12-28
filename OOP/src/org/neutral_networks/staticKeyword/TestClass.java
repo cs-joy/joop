795a31034131f17a901b6a62f22a42683b734cb3
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-28 19:23:58 +0600

    Update TestClass.java

--- OOP/src/org/neutral_networks/staticKeyword/TestClass.java
+++ OOP/src/org/neutral_networks/staticKeyword/TestClass.java
@@ -10,9 +10,15 @@ public class TestClass {
         this.skinColor = skinColor;
     }
 
+    public void print() {
+        System.out.println("print");
+    }
+
     // static method
     public static void printSomething() {
         System.out.println("Printing....");
+        //print(); // Non-static method 'print()' cannot be referenced from a static context
+        //System.out.println(age); // Non-static field 'age' cannot be referenced from a static context
     }
 
     public static String getName() {
