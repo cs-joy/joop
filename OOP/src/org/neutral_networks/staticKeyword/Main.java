2ee2e16d803e826c5a9dd76976a53c7ba00a3859
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-28 18:54:39 +0600

    demonstrate static keyword

--- OOP/src/org/neutral_networks/staticKeyword/Main.java
+++ OOP/src/org/neutral_networks/staticKeyword/Main.java
@@ -2,6 +2,11 @@ package org.neutral_networks.staticKeyword;
 
 public class Main {
     public static void main(String[] args) {
-        
+        TestClass.setName("Mike");
+        // or
+        TestClass.name = "Jim";
+        TestClass testClass = new TestClass(20, "White");
+        //testClass.setName("Mike"); // this is not a right way to set the value to a static variable
+        System.out.println("Name: " + testClass.getName());
     }
 }
---
+++ OOP/src/org/neutral_networks/staticKeyword/TestClass.java
@@ -0,0 +1,36 @@
+package org.neutral_networks.staticKeyword;
+
+public class TestClass {
+    public static String name;
+    public int age;
+    public String skinColor;
+
+    public TestClass(int age, String skinColor) {
+        this.age = age;
+        this.skinColor = skinColor;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        TestClass.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+}
