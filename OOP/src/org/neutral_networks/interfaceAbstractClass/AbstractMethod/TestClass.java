4c5da1350b554bd5568676d0d793f0b1f0850fbe
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-29 16:51:34 +0600

    Create TestClass.java

---
+++ OOP/src/org/neutral_networks/interfaceAbstractClass/AbstractMethod/TestClass.java
@@ -0,0 +1,8 @@
+package org.neutral_networks.interfaceAbstractClass.AbstractMethod;
+
+public class TestClass extends testAbstractClass { // you can't extends multiple abstract class
+    @Override
+    public void printName(String name) {
+
+    }
+}
