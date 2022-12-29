c58b06898d1345a66ccbdcdf8a5595f7a9d1a0c6
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-29 23:22:56 +0600

    Create Main.java

---
+++ OOP/src/org/neutral_networks/concurrency_Exceptions/Main.java
@@ -0,0 +1,24 @@
+package org.neutral_networks.concurrency_Exceptions;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //System.out.println("Hello from another thread");
+                // worker thread
+                for(int i=0; i<5; i++) {
+                    System.out.println("Printing " + i + " in a worker thread.");
+                    //Thread.sleep(1000);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException err) {
+                        err.printStackTrace();
+                    }
+                }
+            }
+        });
+        // main thread
+        thread.start();
+    }
+}
