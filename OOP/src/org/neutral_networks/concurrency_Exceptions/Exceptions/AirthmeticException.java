3eaafc6b3e51a316a2bff80251ee3c6594de567d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-29 23:39:57 +0600

    Update AirthmeticException.java

--- OOP/src/org/neutral_networks/concurrency_Exceptions/Exceptions/AirthmeticException.java
+++ OOP/src/org/neutral_networks/concurrency_Exceptions/Exceptions/AirthmeticException.java
@@ -1,4 +1,23 @@
 package org.neutral_networks.concurrency_Exceptions.Exceptions;
 
 public class AirthmeticException {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 0;
+
+        // except only arithmetic exception to use "ArithmeticException"
+        try {
+            System.out.println(a/b);
+        } catch (ArithmeticException e) {
+            System.out.println("b was zero");
+        }
+
+        // except all the exceptions to use "Exception"
+        try {
+            System.out.println(a/b);
+        } catch (Exception e) {
+            System.out.println("B was zero");
+        }
+    }
+
 }
