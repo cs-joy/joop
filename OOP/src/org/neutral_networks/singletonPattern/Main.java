4341b41f860649639158050d19b5bd0f6751e40d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-30 22:47:06 +0600

    Update Main.java

--- OOP/src/org/neutral_networks/singletonPattern/Main.java
+++ OOP/src/org/neutral_networks/singletonPattern/Main.java
@@ -1,7 +1,12 @@
 package org.neutral_networks.singletonPattern;
 
+import javax.xml.crypto.Data;
+
 public class Main {
     public static void main(String[] args) {
+        Database database = Database.getInstance("Jahangir");
+
+        Database testDatabase = new Database("JS");
 
     }
 }
