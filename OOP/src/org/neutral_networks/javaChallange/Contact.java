43954c439e3d4efec1cf7af8a09a9591030e2a71
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 19:11:46 +0600

    definition of our contact

---
+++ OOP/src/org/neutral_networks/javaChallange/Contact.java
@@ -0,0 +1,64 @@
+package org.neutral_networks.javaChallange;
+
+import java.util.ArrayList;
+
+public class Contact {
+    private String name;
+    private String number;
+    private String email;
+    private ArrayList<String> messages;
+
+    public Contact(String name, String number, String email, ArrayList<String> messages) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.messages = messages;
+    }
+
+    // create new contact and those contact for the first time doesn't have any messages, so generally we ignore the message
+
+    public Contact(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.messages = new ArrayList<>(); // however contact is a new hence we just create new message list and what is empty array list
+    }
+
+    public void getDetails() {
+        System.out.println("Name: " + this.name + "" +
+                           "\nNumber: " + this.number + "" +
+                           "\nEmail: " + this.email);
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<String> messages) {
+        this.messages = messages;
+    }
+}
