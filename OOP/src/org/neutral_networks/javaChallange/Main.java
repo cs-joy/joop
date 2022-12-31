1065177767bf293a0da3facbdd81982c77cf5771
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-01 00:33:54 +0600

    ConcurrentModificationException happing with delete method

--- OOP/src/org/neutral_networks/javaChallange/Main.java
+++ OOP/src/org/neutral_networks/javaChallange/Main.java
@@ -182,9 +182,9 @@ public class Main {
             for (Contact c: contacts) {
                 if (c.getName().equals(name)) {
                     doesExist = true;
-                    Contact dcontact = c;
-                    //contacts.remove(c);
-                    contacts.remove(dcontact);
+                    //Contact dcontact = c;
+                    contacts.remove(c);
+                    //contacts.remove(dcontact);
                 }
             }
 
