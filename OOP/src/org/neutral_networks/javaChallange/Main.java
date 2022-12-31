b70bf6383edd6e0650345643e66548599435e5be
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 18:50:48 +0600

    Create Main.java
    *  Simulate phone's contact and messages applications

---
+++ OOP/src/org/neutral_networks/javaChallange/Main.java
@@ -0,0 +1,23 @@
+package org.neutral_networks.javaChallange;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Simulate your phone's contact and messages applications
+         *
+         * Greet the user
+         * Show these 3 options: 1. Manage contacts     2. Messages     3. Quit
+         * In case of selection 1 --> show these options:
+         *         1. Show all contacts
+         *         2. Add a new contact
+         *         3. Search for a contact
+         *         4. Delete a contact
+         *         5. Go back to the previous menu
+         * In case of 2  --> Show these options:
+         *         1. See the list of all messages
+         *         2. Send a new message
+         *         3. Go back to the previous menu
+         * In case of 3  --> Quit the application
+         */
+    }
+}
